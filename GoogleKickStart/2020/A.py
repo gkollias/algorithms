c814fbadfee184d512a1efa5e8f08e6ab5e130e2
George Kollias <george.kollias@booking.com>
2021-03-19 11:16:38 +0200

    Fix file so we can get it from input

--- GoogleKickStart/2020/A.py
+++ GoogleKickStart/2020/A.py
@@ -1,15 +1,14 @@
 import sys
-with open(sys.argv[1], 'r') as f:
-    x = int(f.readline())
-    for i in range(x):
-        n, money = [int(i) for i in f.readline().split()]
-        
-        l = [int(i) for i in f.readline().split()]
-        l.sort()
-        res = 0
-        for h in l:
-            if money < h:
-                break
-            money -= h
-            res += 1
-        print("Case #" + str(i+1) + ": " + str(res))
+x = int(input())
+for i in range(x):
+    n, money = [int(i) for i in input().split()]
+    
+    l = [int(i) for i in input().split()]
+    l.sort()
+    res = 0
+    for h in l:
+        if money < h:
+            break
+        money -= h
+        res += 1
+    print("Case #" + str(i+1) + ": " + str(res))
